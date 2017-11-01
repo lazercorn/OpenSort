8aa25a7265a887f910f5bc7f3bddf7479e74fc4a
Code2Bits <andre.mare@gmail.com>
2017-11-01 19:11:18 +0200

    Created SelectionSort Algorithm and Test Classes.

---
+++ opensort-core/src/main/java/com/code2bits/opensort/algorithm/SelectionSortAlgorithm.java
@@ -0,0 +1,91 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2017 André Maré
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.code2bits.opensort.algorithm;
+
+
+/**
+ * The SelectionSortAlgorithm class implements the Selection algorithm for sorting an array of integers. 
+ * 
+ * The algorithm divides the input list into two parts. The first sublist is built up from left to right and 
+ * contains items that are already sorted. The second sublist contains the items to be sorted. Initially, the
+ * sorted sublist is empty and the unsorted sublist is the entire input list.  The algorithm starts by 
+ * finding the smallest element in the unsorted sublist, swapping it with the leftmost unsorted element. This
+ * places the element in a sorted position, and moving the sublist boundaries one element to the right.
+ * 
+ * @author	André Maré
+ */
+public final class SelectionSortAlgorithm {
+
+	
+	/**
+	 * The private constructor suppresses the default constructor to prevent the instantiation of class 
+	 * (Ensuring non-instantiability.)
+	 */
+	private SelectionSortAlgorithm() {
+	} 
+	
+	
+	/**
+	 * The sort method is invoked by external classes to sort an array of integers by making use of the 
+	 * SelectionSort algorithm.
+	 */
+	public static void sort(int[] collection) {
+		if (collection != null) {
+			selectionSort(collection);
+		} else {
+			throw new IllegalArgumentException("Input paramenter for array to sort is null.");
+		}
+	}
+	
+	
+	/**
+	 * The method implements the selection sorting algorithm which is all about locating the smallest value 
+	 * from the unsorted part of the array and places it at the position indicated by the iterator.
+	 */
+	private static void selectionSort(int[] collection) {
+		int arrayLength = collection.length;
+		
+		for (int i=0; i < arrayLength; i++) {
+			int min_position = i;
+			
+			for (int j=i+1; j<arrayLength; j++) {
+				if (collection[j] < collection[min_position]) {
+					min_position = j;					
+				}
+			}
+			swap(collection, min_position, i);
+		} 
+	} 
+	
+	
+	/**
+	 * The method swaps two values around within an array based on the two input parameters x and y.
+	 */
+	private static void swap(int[] collection, int x, int y) {
+		int temp = collection[x];
+		collection[x] = collection[y];
+		collection[y] = temp;
+	} 
+	
+}
---
+++ opensort-core/src/test/java/com/code2bits/opensort/algorithm/SelectionSortAlgorithmTest.java
@@ -0,0 +1,111 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2017 André Maré
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.code2bits.opensort.algorithm;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+import com.code2bits.opensort.utils.ArrayUtils;
+
+
+/**
+ * The SelectionSortTest class forms part of the Test Driven Development approach to test the SelectionSort 
+ * class.
+ * 
+ * @author	André Maré
+ */
+public class SelectionSortAlgorithmTest {
+
+	
+	@Test
+	public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+		Constructor<SelectionSortAlgorithm> constructor = SelectionSortAlgorithm.class.getDeclaredConstructor();
+		assertEquals("Modifier is Private.", true, Modifier.isPrivate(constructor.getModifiers()));
+		constructor.setAccessible(true);
+		constructor.newInstance();
+	} 
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSelectionSortNull() {
+		int[] unsortedArray = null;
+		SelectionSortAlgorithm.sort(unsortedArray);
+	} 
+
+	
+	@Test
+	public void testSelectionSortSizeOne() {
+		int[] unsortedArray = ArrayUtils.generateArraySize(1);
+		assertSortedArray(unsortedArray);
+	} 
+	
+	
+	@Test
+	public void testSelectionSortSizeTen() {
+		int[] unsortedArray = ArrayUtils.generateArraySize(10);
+		assertSortedArray(unsortedArray);
+	} 
+
+	
+	@Test
+	public void testSelectionSortSizeTwenty() {
+		int[] unsortedArray = ArrayUtils.generateArraySize(20);
+		assertSortedArray(unsortedArray);
+	} 
+	
+	
+	@Test
+	public void testSelectionSortSizeFifty() {
+		int[] unsortedArray = ArrayUtils.generateArraySize(50);
+		assertSortedArray(unsortedArray);
+	} 
+	
+	
+	@Test
+	public void testSelectionSortSizeHundred() {
+		int[] unsortedArray = ArrayUtils.generateArraySize(100);
+		assertSortedArray(unsortedArray);
+	} 
+	
+	
+	@Test
+	public void testSelectionSortSizeThousand() {
+		int[] unsortedArray = ArrayUtils.generateArraySize(1000);
+		assertSortedArray(unsortedArray);
+	} 
+
+
+	private void assertSortedArray(int[] unsortedArray) {			
+		SelectionSortAlgorithm.sort(unsortedArray);
+		assertEquals("Array is Sorted.", true, ArrayUtils.isSorted(unsortedArray));
+	}  
+	
+	
+}
